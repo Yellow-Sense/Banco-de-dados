5cad99d6f80c8fdfb863017c869af57968bff209
joao-noleto13 <joao.costa@bandtec.com.br>
2021-09-27 21:17:28 -0300

    Adicionei uma tabela e criei as Foreign Key

--- BD_yellow_sensor.sql
+++ BD_yellow_sensor.sql
@@ -1,29 +1,51 @@
 Create Database Yellow_Sensor; 
 
 use Yellow_Sensor;
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-create table  cadastro_cliente (
-id_usuario int primary key auto_increment,
+create table cliente (
+idCliente int primary key auto_increment,
 nome_usuario varchar (50), 
 email_usuario varchar (70) not null,
 telefone varchar (20), 
-senha char (8) not null, 
-nome_granja varchar (50),
-estado char (2)
+senha char (8) not null
 );
 
-insert into cadastro_cliente values
-(null,'Cleber Rosario','cleberson.granja@gmail.com','12345678912','12345678','Galinhas do Cleber','SP');
+insert into cliente values
+	(null,'Cleber Rosario','cleberson.granja@gmail.com','12345678912','12345678','Galinhas do Cleber','SP');
 
-select * from cadastro_cliente;
+select * from cliente;
+
+
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------
+create table login (
+idLogin int primary key auto_increment,
+usuario varchar(45),
+email varchar (45),
+senha char (8)
+) ;
+
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create table granja (
+idGranja int primary key auto_increment,
+nome_granja varchar (50),
+estado char (2),
+cnpj char (14),
+cep char (8),
+numero_propiedade  varchar (4),
+fkCliente int,
+foreign key (fkCliente) references cliente(idCliente)
+);
+
+
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------
 create table sensor(
 	idArea int primary key auto_increment,
     area_granja char(1),
-    dia_hora datetime default current_timestamp,
     temperatura float,
     alerta varchar(20), 
-    constraint alerta_check check (alerta = 'resfriar' or alerta = 'esquentar' or alerta ='ok')
+    constraint alerta_check check (alerta = 'resfriar' or alerta = 'esquentar' or alerta ='ok' or alerta = 'erro')
 )auto_increment = 100000;
 
 insert into sensor(area_granja,temperatura,alerta) values
@@ -38,8 +60,11 @@ select * from sensor;
 create table relatorio_mensal(
 idMensal int primary key auto_increment,
 ovos_mensal int,
-temp_mensal float
+temp_mensal float,
+fkGranja int,
+foreign key (fkGranja) references granja(idGranja)
 )auto_increment = 10000;
+
 
 insert into relatorio_mensal(ovos_mensal,temp_mensal) values 
 (2500,'23.5');
@@ -50,7 +75,9 @@ select * from relatorio_mensal;
 create table relatorio_anual(
 idAnual int primary key auto_increment,
 ovos_anual int,
-temp_anual float
+temp_anual float,
+fkGranja int,
+foreign key (fkGranja) references granja(idGranja)
 )auto_increment = 1000;
 
 insert into relatorio_anual(ovos_anual,temp_anual) values 
@@ -59,18 +86,14 @@ insert into relatorio_anual(ovos_anual,temp_anual) values
 select * from relatorio_mensal;
 
 -- ----------------------------------------------------------------------------------------------------------------------------------------------------
-
-create table login (
-idLogin int primary key auto_increment,
-usuario varchar(45),
-email varchar (45),
-senha char (8)
-) ;
-
-
--- ----------------------------------------------------------------------------------------------------------------------------------------------------
 create table registro (
 idRegistro int primary key auto_increment,
 dia datetime default current_timestamp,
-temperaturas float
+temperaturas float,
+fkGranja int,
+foreign key (fkGranja) references granja(idGranja)
 );
+
+desc registro;
+-- ----------------------------------------------------------------------------------------------------------------------------------------------------
+
